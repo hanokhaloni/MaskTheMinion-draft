d41c8fd9bb2b39eb184ba6af0f26e1ad104a99fc
Hanokh Aloni <hanokh.aloni@gmail.com>
2026-05-03 10:58:33 +0300

    update configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [],
   server: {
     port: 3000,
     host: '0.0.0.0',
